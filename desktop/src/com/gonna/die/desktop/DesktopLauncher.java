b6400cfcb0d114ce5886028a30ac817954bf82cb
Dan McElroy <daniel@mcelroys.net>
2015-01-23 21:32:42 +0000

    Initial commit.

---
+++ desktop/src/com/gonna/die/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gonna.die.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gonna.die.GonnaDieGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GonnaDieGame(), config);
+	}
+}
