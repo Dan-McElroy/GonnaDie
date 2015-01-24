55eed31f2b53a1df6fbbb3996500b78c13d1849a
Dan McElroy <daniel@mcelroys.net>
2015-01-24 21:29:00 +0000

    Added Ship constructor.
    I know.

--- core/src/com/gonna/die/Ship.java
+++ core/src/com/gonna/die/Ship.java
@@ -9,6 +9,15 @@ public class Ship {
 
     public ArrayList<Room> rooms;
 
+    private static final int ROOM_HEALTH = 100;
+
+    public Ship() {
+        rooms = new ArrayList<>();
+        for (int i = 0; i < Room.TOTAL; i++) {
+            rooms.add(new Room(i, ROOM_HEALTH));
+        }
+    }
+
     public boolean isGameOver() {
         for (Room room : rooms) {
             // Assuming delta measured in seconds.
--- core/src/com/gonna/die/systems/MissionSystem.java
+++ core/src/com/gonna/die/systems/MissionSystem.java
@@ -24,6 +24,7 @@ public class MissionSystem extends IteratingSystem {
     public MissionSystem() {
         super(Family.getFor(MissionComponent.class));
         mcm = ComponentMapper.getFor(MissionComponent.class);
+        ship = new Ship();
     }
 
     @Override
