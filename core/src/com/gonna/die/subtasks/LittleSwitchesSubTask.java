0813b5155b995aac5ade971aaf6de082e1594fb1
Robert Clipsham <robert@octarineparrot.com>
2015-01-25 05:46:54 +0000

    Temporarily comment out little switches sub task stuff.

--- core/src/com/gonna/die/subtasks/LittleSwitchesSubTask.java
+++ core/src/com/gonna/die/subtasks/LittleSwitchesSubTask.java
@@ -14,29 +14,29 @@ public class LittleSwitchesSubTask extends SubTask {
     public LittleSwitchesSubTask(Module m) {
         super(m, "Little Switches");
 
-        this.switchesMustBe = new boolean[4];
-        for (int i = 0; i < 4; i++) {
-            Part thing = Device.getInstance().getModulePartByType(this.module.getType(), PartType.SWITCH, i);
-            this.switchesMustBe[i] = thing.getDigitalValue();
-        }
+        //this.switchesMustBe = new boolean[4];
+        //for (int i = 0; i < 4; i++) {
+        //    Part thing = Device.getInstance().getModulePartByType(this.module.getType(), PartType.SWITCH, i);
+        //    this.switchesMustBe[i] = thing.getDigitalValue();
+        //}
 
         System.out.println("im new");
     }
 
     @Override
     public boolean isCompleted() {
-        for (int i = 0; i < 4; i++) {
-            boolean whatIsItNow = Device.getInstance().getModulePartByType(this.module.getType(), PartType.SWITCH, i).getDigitalValue();
-
-            System.out.println(this.switchesMustBe[i]);
-            System.out.println(whatIsItNow);
-            System.out.println();
-
-            if (whatIsItNow != this.switchesMustBe[i]) {
-                System.out.println("REKT");
-                return false;
-            }
-        }
+//        for (int i = 0; i < 4; i++) {
+//            boolean whatIsItNow = Device.getInstance().getModulePartByType(this.module.getType(), PartType.SWITCH, i).getDigitalValue();
+//
+//            System.out.println(this.switchesMustBe[i]);
+//            System.out.println(whatIsItNow);
+//            System.out.println();
+//
+//            if (whatIsItNow != this.switchesMustBe[i]) {
+//                System.out.println("REKT");
+//                return false;
+//            }
+//        }
 
         System.out.println("we good");
         return true;
